6636b6e396514d00b20092f5a5a7e4cfd8455bb4
minha12 <minhha.x89@gmail.com>
2024-12-09 16:09:02 +0100

    Remove commented-out code in utils.py to improve clarity and maintainability

--- src/utils.py
+++ src/utils.py
@@ -151,47 +151,6 @@ def text_under_image(
     return img
 
 
-# def save_images(images, num_rows=1, offset_ratio=0.02, save_path=None, show=False):
-#     if type(images) is list:
-#         num_empty = len(images) % num_rows
-#     elif images.ndim == 4:
-#         num_empty = images.shape[0] % num_rows
-#     else:
-#         images = [images]
-#         num_empty = 0
-
-#     empty_images = np.ones(images[0].shape, dtype=np.uint8) * 255
-#     images = [image.astype(np.uint8) for image in images] + [empty_images] * num_empty
-#     num_items = len(images)
-
-#     h, w, c = images[0].shape
-#     offset = int(h * offset_ratio)
-#     num_cols = num_items // num_rows
-#     image_ = (
-#         np.ones(
-#             (
-#                 h * num_rows + offset * (num_rows - 1),
-#                 w * num_cols + offset * (num_cols - 1),
-#                 3,
-#             ),
-#             dtype=np.uint8,
-#         )
-#         * 255
-#     )
-#     for i in range(num_rows):
-#         for j in range(num_cols):
-#             image_[
-#                 i * (h + offset) : i * (h + offset) + h :,
-#                 j * (w + offset) : j * (w + offset) + w,
-#             ] = images[i * num_cols + j]
-
-#     pil_img = Image.fromarray(image_)
-#     if show:
-#         pil_img.show()
-#     if save_path is not None:
-#         pil_img.save(save_path)
-
-
 def save_images(image, save_path=None):
     # Remove unnecessary dimensions (assuming the first image in batch if batch dimension exists)
     if len(image.shape) > 3:
@@ -229,16 +188,6 @@ def save_results(image, save_path, init_image):
     )
 
     save_images(perturbed, save_path + "_adv_image.png")
-
-    # L1 = LpDistance(1)
-    # L2 = LpDistance(2)
-    # Linf = LpDistance(float("inf"))
-
-    # print(
-    #     "L1: {}\tL2: {}\tLinf: {}".format(
-    #         L1(real, perturbed), L2(real, perturbed), Linf(real, perturbed)
-    #     )
-    # )
 
     diff = perturbed - real
     diff = (diff - diff.min()) / (diff.max() - diff.min())
