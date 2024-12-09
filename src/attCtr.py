598be9f4b15785bdb1fdfb00023f72df85eed193
minha12 <minhha.x89@gmail.com>
2024-12-09 16:08:16 +0100

    refactor module name

---
+++ src/attCtr.py
@@ -0,0 +1,388 @@
+from typing import Union, Tuple
+import numpy as np
+import torch
+import abc
+from torch import optim
+
+from tqdm import tqdm
+
+from src.utils import preprocess
+
+
+class AttentionControl(abc.ABC):
+    def __init__(self):
+        self.cur_step = 0
+        self.num_att_layers = -1
+        self.cur_att_layer = 0
+
+    def between_steps(self):
+        return
+
+    def reset(self):
+        self.cur_step = 0
+        self.cur_att_layer = 0
+
+    @abc.abstractmethod
+    def forward(self, attn, is_cross: bool, place_in_unet: str):
+        raise NotImplementedError
+
+    def __call__(self, attn, is_cross: bool, place_in_unet: str):
+        if self.cur_att_layer >= 0:
+            h = attn.shape[0]
+            self.forward(attn[h // 2 :], is_cross, place_in_unet)
+
+        self.cur_att_layer += 1
+        if self.cur_att_layer == self.num_att_layers:
+            self.cur_att_layer = 0
+            self.cur_step += 1
+            self.between_steps()
+        return attn
+
+
+class AttentionStore(AttentionControl):
+    def __init__(self, res):
+        super(AttentionStore, self).__init__()
+        self.step_store = self.get_empty_store()
+        self.attention_store = {}
+        self.res = res
+
+    @staticmethod
+    def get_empty_store():
+        return {
+            "down_cross": [],
+            "mid_cross": [],
+            "up_cross": [],
+            "down_self": [],
+            "mid_self": [],
+            "up_self": [],
+        }
+
+    def forward(self, attn, is_cross: bool, place_in_unet: str):
+        key = f"{place_in_unet}_{'cross' if is_cross else 'self'}"
+        if attn.shape[1] <= (self.res // 16) ** 2:  # avoid memory overhead
+            self.step_store[key].append(attn)
+        return attn
+
+    def between_steps(self):
+        if len(self.attention_store) == 0:
+            self.attention_store = self.step_store
+        else:
+            for key in self.attention_store:
+                for i in range(len(self.attention_store[key])):
+                    self.attention_store[key][i] = (
+                        self.step_store[key][i] + self.attention_store[key][i]
+                    )
+        self.step_store = self.get_empty_store()
+
+    def get_average_attention(self):
+        average_attention = {
+            key: [item / self.cur_step for item in self.attention_store[key]]
+            for key in self.attention_store
+        }
+        return average_attention
+
+    def reset(self):
+        super(AttentionStore, self).reset()
+        self.step_store = self.get_empty_store()
+        self.attention_store = {}
+
+
+class AttentionControlEdit(AttentionStore, abc.ABC):
+    def __init__(
+        self, num_steps: int, self_replace_steps: Union[float, Tuple[float, float]], res
+    ):
+        super(AttentionControlEdit, self).__init__(res)
+        self.batch_size = 2
+        if type(self_replace_steps) is float:
+            self_replace_steps = 0, self_replace_steps
+        self.num_self_replace = int(num_steps * self_replace_steps[0]), int(
+            num_steps * self_replace_steps[1]
+        )
+        self.loss = 0
+        self.criterion = torch.nn.MSELoss()
+
+    def forward(self, attn, is_cross: bool, place_in_unet: str):
+        super(AttentionControlEdit, self).forward(attn, is_cross, place_in_unet)
+        if is_cross or (
+            self.num_self_replace[0] <= self.cur_step < self.num_self_replace[1]
+        ):
+            h = attn.shape[0] // (self.batch_size)
+            attn = attn.reshape(self.batch_size, h, *attn.shape[1:])
+            attn_base, attn_repalce = attn[0], attn[1:]
+            if not is_cross:
+                self.loss += self.criterion(
+                    attn[1:], self.replace_self_attention(attn_base, attn_repalce)
+                )
+            attn = attn.reshape(self.batch_size * h, *attn.shape[2:])
+        return attn
+
+    def replace_self_attention(self, attn_base, att_replace):
+        return attn_base.unsqueeze(0).expand(att_replace.shape[0], *attn_base.shape)
+
+
+def register_attention_control(model, controller):
+    def ca_forward(self, place_in_unet):
+
+        def forward(x, context=None):
+            q = self.to_q(x)
+            is_cross = context is not None
+            context = context if is_cross else x
+            k = self.to_k(context)
+            v = self.to_v(context)
+            q = self.reshape_heads_to_batch_dim(q)
+            k = self.reshape_heads_to_batch_dim(k)
+            v = self.reshape_heads_to_batch_dim(v)
+
+            sim = torch.einsum("b i d, b j d -> b i j", q, k) * self.scale
+
+            attn = sim.softmax(dim=-1)
+            attn = controller(attn, is_cross, place_in_unet)
+            out = torch.einsum("b i j, b j d -> b i d", attn, v)
+            out = self.reshape_batch_dim_to_heads(out)
+
+            out = self.to_out[0](out)
+            out = self.to_out[1](out)
+            return out
+
+        return forward
+
+    def register_recr(net_, count, place_in_unet):
+        if net_.__class__.__name__ == "CrossAttention":
+            net_.forward = ca_forward(net_, place_in_unet)
+            return count + 1
+        elif hasattr(net_, "children"):
+            for net__ in net_.children():
+                count = register_recr(net__, count, place_in_unet)
+        return count
+
+    cross_att_count = 0
+    sub_nets = model.unet.named_children()
+    for net in sub_nets:
+        if "down" in net[0]:
+            cross_att_count += register_recr(net[1], 0, "down")
+        elif "up" in net[0]:
+            cross_att_count += register_recr(net[1], 0, "up")
+        elif "mid" in net[0]:
+            cross_att_count += register_recr(net[1], 0, "mid")
+    controller.num_att_layers = cross_att_count
+
+
+def reset_attention_control(model):
+    def ca_forward(self):
+        def forward(x, context=None):
+            q = self.to_q(x)
+            is_cross = context is not None
+            context = context if is_cross else x
+            k = self.to_k(context)
+            v = self.to_v(context)
+            q = self.reshape_heads_to_batch_dim(q)
+            k = self.reshape_heads_to_batch_dim(k)
+            v = self.reshape_heads_to_batch_dim(v)
+
+            sim = torch.einsum("b i d, b j d -> b i j", q, k) * self.scale
+
+            attn = sim.softmax(dim=-1)
+            out = torch.einsum("b i j, b j d -> b i d", attn, v)
+            out = self.reshape_batch_dim_to_heads(out)
+
+            out = self.to_out[0](out)
+            out = self.to_out[1](out)
+            return out
+
+        return forward
+
+    def register_recr(net_):
+        if net_.__class__.__name__ == "CrossAttention":
+            net_.forward = ca_forward(net_)
+        elif hasattr(net_, "children"):
+            for net__ in net_.children():
+                register_recr(net__)
+
+    sub_nets = model.unet.named_children()
+    for net in sub_nets:
+        if "down" in net[0]:
+            register_recr(net[1])
+        elif "up" in net[0]:
+            register_recr(net[1])
+        elif "mid" in net[0]:
+            register_recr(net[1])
+
+
+def encoder(image, model, res=512):
+    generator = torch.Generator().manual_seed(8888)
+    image = preprocess(image, res)
+    gpu_generator = torch.Generator(device=image.device)
+    gpu_generator.manual_seed(generator.initial_seed())
+    return 0.18215 * model.vae.encode(image).latent_dist.sample(generator=gpu_generator)
+
+
+@torch.no_grad()
+def ddim_reverse_sample(
+    image,
+    prompt,
+    model,
+    num_inference_steps: int = 20,
+    guidance_scale: float = 2.5,
+    res=512,
+):
+    batch_size = 1
+
+    max_length = 77
+    uncond_input = model.tokenizer(
+        [""] * batch_size,
+        padding="max_length",
+        max_length=max_length,
+        return_tensors="pt",
+    )
+    uncond_embeddings = model.text_encoder(uncond_input.input_ids.to(model.device))[0]
+
+    text_input = model.tokenizer(
+        prompt[0],
+        padding="max_length",
+        max_length=model.tokenizer.model_max_length,
+        truncation=True,
+        return_tensors="pt",
+    )
+    text_embeddings = model.text_encoder(text_input.input_ids.to(model.device))[0]
+
+    context = [uncond_embeddings, text_embeddings]
+    context = torch.cat(context)
+
+    model.scheduler.set_timesteps(num_inference_steps)
+
+    latents = encoder(image, model, res=res)
+    timesteps = model.scheduler.timesteps.flip(0)
+
+    all_latents = [latents]
+
+    #  Not inverse the last step, as the alpha_bar_next will be set to 0 which is not aligned to its real value (~0.003)
+    #  and this will lead to a bad result.
+    for t in tqdm(timesteps[:-1], desc="DDIM_inverse"):
+        latents_input = torch.cat([latents] * 2)
+        noise_pred = model.unet(latents_input, t, encoder_hidden_states=context)[
+            "sample"
+        ]
+
+        noise_pred_uncond, noise_prediction_text = noise_pred.chunk(2)
+        noise_pred = noise_pred_uncond + guidance_scale * (
+            noise_prediction_text - noise_pred_uncond
+        )
+
+        next_timestep = (
+            t
+            + model.scheduler.config.num_train_timesteps
+            // model.scheduler.num_inference_steps
+        )
+        alpha_bar_next = (
+            model.scheduler.alphas_cumprod[next_timestep]
+            if next_timestep <= model.scheduler.config.num_train_timesteps
+            else torch.tensor(0.0)
+        )
+
+        "leverage reversed_x0"
+        reverse_x0 = (
+            1
+            / torch.sqrt(model.scheduler.alphas_cumprod[t])
+            * (latents - noise_pred * torch.sqrt(1 - model.scheduler.alphas_cumprod[t]))
+        )
+
+        latents = (
+            reverse_x0 * torch.sqrt(alpha_bar_next)
+            + torch.sqrt(1 - alpha_bar_next) * noise_pred
+        )
+
+        all_latents.append(latents)
+
+    #  all_latents[N] -> N: DDIM steps  (X_{T-1} ~ X_0)
+    return latents, all_latents
+
+
+def diffusion_step(model, latents, context, t, guidance_scale):
+    latents_input = torch.cat([latents] * 2)
+    noise_pred = model.unet(latents_input, t, encoder_hidden_states=context)["sample"]
+    noise_pred_uncond, noise_prediction_text = noise_pred.chunk(2)
+    noise_pred = noise_pred_uncond + guidance_scale * (
+        noise_prediction_text - noise_pred_uncond
+    )
+    latents = model.scheduler.step(noise_pred, t, latents)["prev_sample"]
+    return latents
+
+
+def latent2image(vae, latents):
+    latents = 1 / 0.18215 * latents
+    image = vae.decode(latents)["sample"]
+    image = (image / 2 + 0.5).clamp(0, 1)
+    image = image.cpu().permute(0, 2, 3, 1).numpy()
+    image = (image * 255).astype(np.uint8)
+    return image
+
+
+def init_latent(latent, model, height, width, batch_size):
+    latents = latent.expand(
+        batch_size, model.unet.in_channels, height // 8, width // 8
+    ).to(model.device)
+    return latent, latents
+
+
+def optimize_unconditional_embeds(
+    model,
+    guidance_scale,
+    start_step,
+    width,
+    height,
+    latent,
+    inversion_latents,
+    init_prompt,
+    batch_size,
+):
+    max_length = 77
+    uncond_input = model.tokenizer(
+        [""] * batch_size,
+        padding="max_length",
+        max_length=max_length,
+        return_tensors="pt",
+    )
+
+    uncond_embeddings = model.text_encoder(uncond_input.input_ids.to(model.device))[0]
+
+    text_input = model.tokenizer(
+        init_prompt,
+        padding="max_length",
+        max_length=model.tokenizer.model_max_length,
+        truncation=True,
+        return_tensors="pt",
+    )
+    text_embeddings = model.text_encoder(text_input.input_ids.to(model.device))[0]
+
+    all_uncond_emb = []
+    latent, latents = init_latent(latent, model, height, width, batch_size)
+
+    uncond_embeddings.requires_grad_(True)
+    optimizer = optim.AdamW([uncond_embeddings], lr=1e-1)
+    loss_func = torch.nn.MSELoss()
+
+    context = torch.cat([uncond_embeddings, text_embeddings])
+
+    #  The DDIM should begin from 1, as the inversion cannot access X_T but only X_{T-1}
+    for ind, t in enumerate(
+        tqdm(
+            model.scheduler.timesteps[1 + start_step - 1 :],
+            desc="Optimize_uncond_embed",
+        )
+    ):
+        for _ in range(10 + 2 * ind):
+            out_latents = diffusion_step(model, latents, context, t, guidance_scale)
+            optimizer.zero_grad()
+            loss = loss_func(out_latents, inversion_latents[start_step - 1 + ind + 1])
+            loss.backward()
+            optimizer.step()
+
+            context = [uncond_embeddings, text_embeddings]
+            context = torch.cat(context)
+
+        with torch.no_grad():
+            latents = diffusion_step(
+                model, latents, context, t, guidance_scale
+            ).detach()
+            all_uncond_emb.append(uncond_embeddings.detach().clone())
+    return latent, uncond_embeddings, all_uncond_emb
